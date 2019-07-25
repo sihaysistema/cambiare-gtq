94ba8577443d430e6d1aa277283e3ef925b8ec62
monroy95 <m.monroyc22@gmail.com>
2019-07-25 03:49:26 +0000

    First commit

---
+++ cambiare_gtq/hooks.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from . import __version__ as app_version
+
+app_name = "cambiare_gtq"
+app_title = "Cambiare GTQ"
+app_publisher = "Si Hay Sistema"
+app_description = "Cambio de moneda"
+app_icon = "octicon octicon-graph"
+app_color = "#27AE60"
+app_email = "m.monroyc22@gmail.com"
+app_license = "GNU General Public License v3.0"
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/cambiare_gtq/css/cambiare_gtq.css"
+# app_include_js = "/assets/cambiare_gtq/js/cambiare_gtq.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/cambiare_gtq/css/cambiare_gtq.css"
+# web_include_js = "/assets/cambiare_gtq/js/cambiare_gtq.js"
+
+# include js in page
+# page_js = {"page" : "public/js/file.js"}
+
+# include js in doctype views
+# doctype_js = {"doctype" : "public/js/doctype.js"}
+# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
+# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+#	"Role": "home_page"
+# }
+
+# Website user home page (by function)
+# get_website_user_home_page = "cambiare_gtq.utils.get_home_page"
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# Installation
+# ------------
+
+# before_install = "cambiare_gtq.install.before_install"
+# after_install = "cambiare_gtq.install.after_install"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "cambiare_gtq.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+#	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"cambiare_gtq.tasks.all"
+# 	],
+# 	"daily": [
+# 		"cambiare_gtq.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"cambiare_gtq.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"cambiare_gtq.tasks.weekly"
+# 	]
+# 	"monthly": [
+# 		"cambiare_gtq.tasks.monthly"
+# 	]
+# }
+
+# Testing
+# -------
+
+# before_tests = "cambiare_gtq.install.before_tests"
+
+# Overriding Whitelisted Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "cambiare_gtq.event.get_events"
+# }
+
