22cf6ce5781faeefd4f0390e1ef8991e2b1dbbea
monroy95 <m.monroyc22@gmail.com>
2021-01-04 21:55:59 +0000

    WIP adding filters, query

--- cambiare_gtq/cambiare_gtq/doctype/cambiare_cheque_print_set/cambiare_cheque_print_set.js
+++ cambiare_gtq/cambiare_gtq/doctype/cambiare_cheque_print_set/cambiare_cheque_print_set.js
@@ -2,7 +2,42 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Cambiare Cheque Print Set', {
-	// refresh: function(frm) {
+    onload: function (frm) {
+        // Filtro para generar listado solo de cuentas bancarias
+        // que sean:
+        frm.set_query('source_bank_account', () => {
+            return {
+                filters: {
+                    is_company_account: 1,
+                    company: frm.doc.company
+                }
+            };
+        });
+        cur_frm.refresh_field('source_bank_account');
+    },
+    get_cheque_payments: function (frm) {
+        console.log('Hola Mundo');
 
-	// }
+        frappe.call({
+            method: 'cambiare_gtq.cambiare_gtq.doctype.cambiare_cheque_print_set.cambiare_cheque_print_set.get_data',
+            args: {
+                filters: {
+                    company: frm.doc.company,
+                    source_bank_acc: frm.doc.source_bank_account,
+                    start_date: frm.doc.start_date,
+                    end_date: frm.doc.end_date,
+                    currency: frm.doc.currency
+                },
+            },
+            // freeze: true,
+            // freeze_message: __('Generando reporte, por favor espere...'),
+            callback: function (r) {
+                // if (!r.exc) {
+                //     clearInterval(frm.page['interval']);
+                //     frm.reload_doc();
+                // }
+                console.log(r.message);
+            }
+        });
+    }
 });
--- cambiare_gtq/cambiare_gtq/doctype/cambiare_cheque_print_set/cambiare_cheque_print_set.py
+++ cambiare_gtq/cambiare_gtq/doctype/cambiare_cheque_print_set/cambiare_cheque_print_set.py
@@ -3,8 +3,19 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+
+import frappe
+from frappe import _, _dict
 from frappe.model.document import Document
+import json
+
 
 class CambiareChequePrintSet(Document):
-	pass
+    pass
+
+
+@frappe.whitelist()
+def get_data(params):
+
+    filters = frappe._dict(json.loads(params))
+
