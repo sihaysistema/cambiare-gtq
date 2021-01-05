b303ed7c0e10e5d9e3c4678ff79c59c4715da1ca
ronaldo ramirez <perencen17@gmail.com>
2021-01-05 22:17:14 +0000

    Agregando Cheque Print Set Bank Account

--- cambiare_gtq/cambiare_gtq/doctype/cambiare_cheque_print_set/cambiare_cheque_print_set.js
+++ cambiare_gtq/cambiare_gtq/doctype/cambiare_cheque_print_set/cambiare_cheque_print_set.js
@@ -2,7 +2,10 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Cambiare Cheque Print Set', {
-	// refresh: function(frm) {
-
-	// }
+    refresh: function(frm) {
+        frm.add_custom_button(__("Get Cheque Payments"),
+            function () {
+            
+            }).addClass("btn-primary");
+    }
 });
