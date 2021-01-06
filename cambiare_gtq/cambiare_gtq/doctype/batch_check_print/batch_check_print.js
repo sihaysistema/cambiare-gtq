b696254fed56254f121b56a05456ed09e7a66d72
monroy95 <m.monroyc22@gmail.com>
2021-01-06 22:07:56 +0000

    #20 OK generacion lotes

--- cambiare_gtq/cambiare_gtq/doctype/batch_check_print/batch_check_print.js
+++ cambiare_gtq/cambiare_gtq/doctype/batch_check_print/batch_check_print.js
@@ -2,7 +2,63 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Batch Check Print', {
-	// refresh: function(frm) {
-
-	// }
+    onload_post_render: function (frm) {
+        frm.get_field("generate_print_sets").$input.addClass("btn btn-primary"); //" fa fa-upload");
+    },
+    generate_print_sets: function (frm) {
+        let status_data = []
+        if (frm.doc.check_to_print.length > 0) {
+            var status_crea = ''
+            // Por cada fila de tabla hija
+            frm.doc.check_to_print.forEach(element => {
+                let params = {
+                    company: frm.doc.company,
+                    source_bank_acc: element.bank_account,
+                    start_date: frm.doc.transaction_start_date,
+                    end_date: frm.doc.transaction_end_date,
+                    currency: element.bank_account_currency
+                }
+                status_crea = generate_prints(frm, params, element.idx);
+                console.log(status_crea);
+            });
+        }
+    }
 });
+
+
+function generate_prints(frm, params, idx) {
+    // Obtencion de datos
+    frappe.call({
+        method: 'cambiare_gtq.cambiare_gtq.doctype.cambiare_cheque_print_set.cambiare_cheque_print_set.get_data',
+        args: {
+            params: params
+        },
+        callback: function (r) {
+            if (r.message.length == 0) {
+                frappe.msgprint(__('Registro para la fila') + ` <b> ${idx}</b>,` + __('no creado, no se encontraron datos segun los parametros seleccionados'));
+                return
+            } else {
+                // Creacion de registros, si hay data!
+                frappe.call({
+                    method: 'cambiare_gtq.cambiare_gtq.doctype.batch_check_print.batch_check_print.create_set',
+                    args: {
+                        params: params,
+                        data_reg: r.message
+                    },
+                    callback: function (r) {
+                        if (r.message[0]) {
+                            frappe.msgprint(__('Registro para la fila') + `<b> ${idx} </b>,` + __('creado, puedes verlo en el siguiente link') +
+                                ` <p><a href="#Form/Cambiare%20Cheque%20Print%20Set/${r.message[1]}" target="_blank">Ver</a>.</p>`);
+                            return
+                        } else {
+                            frappe.msgprint(__('Registro para la fila') + ` <b> ${idx} </b>,` +
+                                __(`no creado, no se encontraron datos segun los parametros seleccionados,
+                                 mas detalles en el siguiente log \n`) + `<code> ${r.message[1]} </code>`);
+                            return
+                        }
+                    }
+                });
+            }
+        }
+    });
+}
--- cambiare_gtq/cambiare_gtq/doctype/batch_check_print/batch_check_print.py
+++ cambiare_gtq/cambiare_gtq/doctype/batch_check_print/batch_check_print.py
@@ -3,8 +3,49 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+
+import json
+from datetime import date
+
+import frappe
 from frappe.model.document import Document
 
 class BatchCheckPrint(Document):
-	pass
+    pass
+
+
+@frappe.whitelist()
+def create_set(params, data_reg):
+    """
+    Creador de registros para Cambiare Cheque Print Set
+
+    Args:
+        params (dict): Filtros batch doctype
+        data_reg (list): Listado de registros para tabla hija
+
+    Returns:
+        bool: True/False
+    """
+    try:
+        today = date.today()
+
+        params = json.loads(params)
+        data_reg = json.loads(data_reg)
+
+        # create a new document
+        doc = frappe.get_doc({
+            'doctype': 'Cambiare Cheque Print Set',
+            'creation_date': today.strftime("%Y-%m-%d"),
+            'source_bank_account': params.get('source_bank_acc'),
+            'start_date': params.get('start_date'),
+            'company': params.get('company'),
+            'currency': params.get('currency'),
+            'end_date': params.get('end_date'),
+            'cheque_to_print': data_reg,
+        })
+        doc.save()
+
+        return True, doc.name
+
+    except:
+        return False, frappe.get_traceback()
