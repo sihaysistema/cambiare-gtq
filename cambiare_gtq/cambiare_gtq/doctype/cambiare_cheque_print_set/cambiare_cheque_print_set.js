5de3f460a1b154e5d344e41b1867614cc024bd3a
monroy95 <m.monroyc22@gmail.com>
2021-01-14 16:47:37 +0000

    Comentando codigo para boton no usado en cambiare chque print set

--- cambiare_gtq/cambiare_gtq/doctype/cambiare_cheque_print_set/cambiare_cheque_print_set.js
+++ cambiare_gtq/cambiare_gtq/doctype/cambiare_cheque_print_set/cambiare_cheque_print_set.js
@@ -56,64 +56,65 @@ frappe.ui.form.on('Cambiare Cheque Print Set', {
             }
         });
     },
-    refresh: function (frm) {
-        frm.add_custom_button(__('Generar Lotes PDF'), () => {
-            let data_je = [];
-            let data_pe = [];
+    // Descomentar si se desea usar el API para generador masivo y descarga automatica
+    // refresh: function (frm) {
+    //     frm.add_custom_button(__('Generar Lotes PDF'), () => {
+    //         let data_je = [];
+    //         let data_pe = [];
 
-            frm.doc.cheque_to_print.forEach(element => {
-                if (element.transaction_id == 'Journal Entry') {
-                    data_je.push(element.id);
-                }
+    //         frm.doc.cheque_to_print.forEach(element => {
+    //             if (element.transaction_id == 'Journal Entry') {
+    //                 data_je.push(element.id);
+    //             }
 
-                if (element.transaction_id == 'Payment Entry') {
-                    data_pe.push(element.id);
-                }
-            });
+    //             if (element.transaction_id == 'Payment Entry') {
+    //                 data_pe.push(element.id);
+    //             }
+    //         });
 
-            const data_per_doctype = [
-                { doctype: 'Payment Entry', valores: data_pe },
-                { doctype: 'Journal Entry', valores: data_je }
-            ]
+    //         const data_per_doctype = [
+    //             { doctype: 'Payment Entry', valores: data_pe },
+    //             { doctype: 'Journal Entry', valores: data_je }
+    //         ]
 
-            const dialog = new frappe.ui.Dialog({
-                title: __('Print Documents'),
-                fields: [{
-                    'fieldtype': 'Check',
-                    'label': __('With Letterhead'),
-                    'fieldname': 'with_letterhead'
-                },
-                {
-                    'fieldtype': 'Select',
-                    'label': __('Print Format'),
-                    'fieldname': 'print_sel',
-                    options: frappe.meta.get_print_formats(frm.doc.doctype)
-                }]
-            });
+    //         const dialog = new frappe.ui.Dialog({
+    //             title: __('Print Documents'),
+    //             fields: [{
+    //                 'fieldtype': 'Check',
+    //                 'label': __('With Letterhead'),
+    //                 'fieldname': 'with_letterhead'
+    //             },
+    //             {
+    //                 'fieldtype': 'Select',
+    //                 'label': __('Print Format'),
+    //                 'fieldname': 'print_sel',
+    //                 options: frappe.meta.get_print_formats(frm.doc.doctype)
+    //             }]
+    //         });
 
-            dialog.set_primary_action(__('Print'), args => {
-                if (!args) return;
-                // Primero se imprimen los cheques para payment entry
-                for (const iterator of data_per_doctype) {
-                    const default_print_format = 'Standard'; // frappe.get_meta(iterator).default_print_format;
-                    const with_letterhead = args.with_letterhead ? 1 : 0;
-                    const print_format = args.print_sel ? args.print_sel : 'Standard'; //default_print_format;
-                    const json_string = JSON.stringify(iterator.valores);
+    //         dialog.set_primary_action(__('Print'), args => {
+    //             if (!args) return;
+    //             // Primero se imprimen los cheques para payment entry
+    //             for (const iterator of data_per_doctype) {
+    //                 const default_print_format = 'Standard'; // frappe.get_meta(iterator).default_print_format;
+    //                 const with_letterhead = args.with_letterhead ? 1 : 0;
+    //                 const print_format = args.print_sel ? args.print_sel : 'Standard'; //default_print_format;
+    //                 const json_string = JSON.stringify(iterator.valores);
 
-                    const w = window.open('/api/method/frappe.utils.print_format.download_multi_pdf?' +
-                        'doctype=' + encodeURIComponent(iterator.doctype) +
-                        '&name=' + encodeURIComponent(json_string) +
-                        '&format=' + encodeURIComponent(print_format) +
-                        '&no_letterhead=' + (with_letterhead ? '0' : '1'));
-                    if (!w) {
-                        frappe.msgprint(__('Please enable pop-ups'));
-                        return;
-                    }
-                }
-            });
+    //                 const w = window.open('/api/method/frappe.utils.print_format.download_multi_pdf?' +
+    //                     'doctype=' + encodeURIComponent(iterator.doctype) +
+    //                     '&name=' + encodeURIComponent(json_string) +
+    //                     '&format=' + encodeURIComponent(print_format) +
+    //                     '&no_letterhead=' + (with_letterhead ? '0' : '1'));
+    //                 if (!w) {
+    //                     frappe.msgprint(__('Please enable pop-ups'));
+    //                     return;
+    //                 }
+    //             }
+    //         });
 
-            dialog.show();
+    //         dialog.show();
 
-        }).addClass('btn btn-primary');
-    }
+    //     }).addClass('btn btn-primary');
+    // }
 });
